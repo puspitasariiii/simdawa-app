387df29f3c58a7d09fd01a8e8cccf16952a31cb3
gisyaauliaa <gisyaaulia272@gmail.com>
2025-01-13 13:01:16 +0700

    Added Report Prodi n Notifikasi

--- application/views/persyaratan/persyaratan_create.php
+++ application/views/persyaratan/persyaratan_create.php
@@ -3,12 +3,12 @@
         <div class="row">
             <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                 <div class="page-header">
-                    <h2 class="pageheader-title">Tambah Data Jenis Beasiswa </h2>
+                    <h2 class="pageheader-title">Tambah Data Persyaratan</h2>
                     <div class="page-breadcrumb">
                         <nav aria-label="breadcrumb">
                             <ol class="breadcrumb">
                                 <li class="breadcrumb-item"><a href="<?= base_url('home') ?>" class="breadcrumb-link">Dashboard</a></li>
-                                <li class="breadcrumb-item"><a href="<?= base_url('jenis') ?>" class="breadcrumb-link">Jenis Beasiswa</a></li>
+                                <li class="breadcrumb-item"><a href="<?= base_url('persyaratan') ?>" class="breadcrumb-link">Persyaratan</a></li>
                                 <li class="breadcrumb-item active" aria-current="page">Tambah Data</li>
                             </ol>
                         </nav>
@@ -23,14 +23,14 @@
                 ?>
                 <div class="card">
                     <div class="card-header">
-                        Tambah Data Jenis Beasiswa
+                        Tambah Data Persyaratan
                     </div>
                     <div class="card-body">
                         <form action="" method="post">
                             <div class="form-group row">
-                                <label for="nama_jenis" class="col-md-2">Nama Jenis Beasiswa</label>
+                                <label for="nama_persyaratan" class="col-md-2">Nama Persyaratan</label>
                                 <div class="col-md-10">
-                                    <input type="text" name="nama_jenis" required placeholder="Nama Jenis Beasiswa" class="form-control">
+                                    <input type="text" name="nama_persyaratan" required placeholder="Nama Persyaratan" class="form-control">
                                 </div>
                             </div>
                             <div class="form-group row">
@@ -39,7 +39,7 @@
                                     <input type="text" name="keterangan" required placeholder="Keterangan" class="form-control">
                                 </div>
                             </div>
-                            <a href="<?= base_url('jenis') ?>" class="btn btn-sm btn-danger float-right">Batal</a>
+                            <a href="<?= base_url('persyaratan') ?>" class="btn btn-sm btn-danger float-right">Batal</a>
                             <button type="submit" name="create" class="btn btn-sm btn-info float-right mr-1">Simpan</button>
                         </form>
                     </div>
--- application/views/persyaratan/persyaratan_read.php
+++ application/views/persyaratan/persyaratan_read.php
@@ -3,13 +3,12 @@
         <div class="row">
             <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                 <div class="page-header">
-                    <h2 class="pageheader-title">Data Jenis Beasiswa </h2>
+                    <h2 class="pageheader-title">Data Persyaratan</h2>
                     <div class="page-breadcrumb">
                         <nav aria-label="breadcrumb">
                             <ol class="breadcrumb">
                                 <li class="breadcrumb-item"><a href="<?= base_url('home') ?>" class="breadcrumb-link">Dashboard</a></li>
-                                <li class="breadcrumb-item"><a href="<?= base_url('jenis') ?>" class="breadcrumb-link">Jenis Beasiswa</a></li>
-                                <li class="breadcrumb-item active" aria-current="page">Data Jenis Beasiswa</li>
+                                <li class="breadcrumb-item active" aria-current="page">Data Persyaratan</li>
                             </ol>
                         </nav>
                     </div>
@@ -23,34 +22,31 @@
                 ?>
                 <div class="card">
                     <div class="card-header">
-                        <a href="<?= base_url('jenis/tambah') ?>" class="btn btn-sm btn-success float-right"><i class="fas fa-plus">Tambah Data</i></a>
+                        <a href="<?= base_url('persyaratan/tambah') ?>" class="btn btn-sm btn-success float-right"><i class="fas fa-plus"></i> Tambah Data</a>
                     </div>
                     <div class="card-body">
-                        <table class="table table-bordered" id="mytabel">
+                        <table class="table table-bordered" id="myTable">
                             <thead>
                                 <tr>
                                     <th>No</th>
-                                    <th>Jenis Beasiswa</th>
+                                    <th>Nama Persyaratan</th>
                                     <th>Keterangan</th>
                                     <th>Aksi</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <?php $no = 1;
-                                foreach ($jenis as $a) {
-                                ?>
+                                foreach ($persyaratan as $p) : ?>
                                     <tr>
-                                        <td><?= $no++ ?></td>
-                                        <td><?= $a->nama_jenis ?></td>
-                                        <td><?= $a->keterangan ?></td>
+                                        <td><?= $no++; ?></td>
+                                        <td><?= $p->nama_persyaratan; ?></td>
+                                        <td><?= $p->keterangan; ?></td>
                                         <td>
-                                            <a href="<?= base_url('jenis/ubah/' . $a->id) ?>" class="btn btn-sm btn-info"><i class="fas fa-edit"></i> Ubah</a>
-                                            <a href="<?= base_url('jenis/hapus/' . $a->id) ?>" class="btn btn-sm btn-danger" onclick="return confirm('Ingin hapus data ini?')"><i class="fas fa-trash"></i> Hapus</a>
+                                            <a href="<?= base_url('persyaratan/ubah/' . $p->id) ?>" class="btn btn-sm btn-info"><i class="fas fa-edit"></i> Ubah</a>
+                                            <a href="<?= base_url('persyaratan/hapus/' . $p->id) ?>" class="btn btn-sm btn-danger" onclick="return confirm('Ingin menghapus data ini?')"><i class="fas fa-trash"></i> Hapus</a>
                                         </td>
                                     </tr>
-                                <?php
-                                }
-                                ?>
+                                <?php endforeach; ?>
                             </tbody>
                         </table>
                     </div>
